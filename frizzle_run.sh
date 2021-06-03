d57f676eb22d30e2daddca29fc158526dab3575d
gauthamys <gauthamys123@gmail.com>
2021-06-03 00:30:32 +0000

    new file names and error handling

---
+++ frizzle_run.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+export PATH="/usr/bin/python2.7:$PATH"
+
+Connect(){
+	sudo pon rnet
+	echo Connecting to the internet...
+	sleep 1m
+	echo Connected
+}
+
+Working(){
+	python /home/pi/Desktop/edge/sync_id.py
+	while true
+ 	do
+ 		python /home/pi/Desktop/edge/frizzle.py
+ 		sleep 30
+ 	done
+}
+
+
+#/sbin/ifconfig ppp0
+path=$(curl -X GET "http://13.126.242.56:80")
+echo $path
+if [ -z "$path" ]; 
+then
+	echo not connected
+	Connect
+	Working
+else
+	echo connected
+	Working
+fi
