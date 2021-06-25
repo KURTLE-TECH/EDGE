79ee72c1e44982f14e46a8c677547cfb7081d510
A Anantha Krishna <work.krishna2001@gmail.com>
2021-06-25 21:49:33 +0000

    added failsafe for register node

--- sync_id.py
+++ sync_id.py
@@ -1,22 +1,30 @@
 import requests
 import os
 from json import dumps
-f = open('/home/pi/Desktop/EDGE/ID.txt','r')
-id = f.read()
-f.close()
-payload = {'Device ID':id}
-r = requests.post('https://api.frizzleweather.com/node/initialise',verify = False, data=dumps(payload))
+from time import sleep
 
-data = r.json()
-print(data)
-date = data['date']
-time = data['time']
-
-if 'Device ID' in data.keys():
-	f = open('/home/pi/Desktop/EDGE/ID.txt','w')
-	uid = data['Device ID']
-	f.write(uid)
+def verify_id():
+	f = open("/home/pi/Desktop/EDGE/ID.txt","r")
+	id = f.read()
 	f.close()
-os.system('sudo timedatectl set-ntp false')
-os.system('sudo timedatectl set-time \"{0} {1}\"'.format(date,time))
+	payload = {'Device ID':id}
+	print(payload)
+	r = requests.post('https://api.frizzleweather.com/node/initialise',verify=False, data = dumps(payload))
+	data = r.json()
+	print(data)
+	if 'date' in data.keys():
+		date = data['date']
+		#print(date)
+		time = data['time']
+		#print(time)
+		os.system('sudo timedatectl set-ntp false')
+		os.system('sudo timedatectl set-time \"{0} {1}\"'.format(date,time))
+		#print('cool')
+		return [date,time]
+	else:
+		print('trying again')
+		sleep(3)
+		verify_id()
+
+verify_id()
 
